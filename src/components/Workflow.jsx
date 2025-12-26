432ba34dc8a41d4f239a5f09ea7358ecaa1fbe8a
ala-bera-abjexperts <alabera.abjexperts@gmail.com>
2025-12-26 15:07:27 +0530

    features: undo and redo nodes

--- src/components/Workflow.jsx
+++ src/components/Workflow.jsx
@@ -5,8 +5,13 @@ import {
   ReactFlowProvider,
   useReactFlow,
   useNodesState,
+  Position,
+  Handle,
+  useKeyPress,
 } from "@xyflow/react";
 import "@xyflow/react/dist/style.css";
+import { useEffect } from "react";
+import { getStraightPath } from "@xyflow/react";
 
 const CustomNode = ({ data }) => (
   <div
@@ -18,14 +23,64 @@ const CustomNode = ({ data }) => (
     }}
   >
     <strong>{data.label}</strong>
+    {/* Targets */}
+    <Handle type="target" position={Position.Top} id="t-top" />
+    <Handle type="target" position={Position.Right} id="t-right" />
+    <Handle type="target" position={Position.Bottom} id="t-bottom" />
+    <Handle type="target" position={Position.Left} id="t-left" />
+
+    {/* Sources */}
+    <Handle type="source" position={Position.Top} id="s-top" />
+    <Handle type="source" position={Position.Right} id="s-right" />
+    <Handle type="source" position={Position.Bottom} id="s-bottom" />
+    <Handle type="source" position={Position.Left} id="s-left" />
   </div>
 );
+
+export function CustomEdge({ id, sourceX, sourceY, targetX, targetY }) {
+  const [edgePath] = getStraightPath({
+    sourceX,
+    sourceY,
+    targetX,
+    targetY,
+  });
+
+  return (
+    <>
+      <BaseEdge id={id} path={edgePath} />
+    </>
+  );
+}
 
 const nodeTypes = { custom: CustomNode };
 
 const FlowInner = () => {
-  const [nodes, setNodes, onNodesChange] = useNodesState([]);
+  const [nodes, setNodes, onNodesChange] = useNodesState(
+    JSON.parse(localStorage.getItem("nodes"))?.slice(
+      0,
+      Number(localStorage.getItem("currentIndex"))
+    ) || []
+  );
   const { screenToFlowPosition } = useReactFlow(); // Needs ReactFlowProvider
+  const cmdAndZPress = useKeyPress("Meta+z", "Strg+z");
+  const cmdAndYPress = useKeyPress("Meta+y", "Strg+y");
+  console.log("Cmd and z is press :", cmdAndZPress);
+
+  useEffect(() => {
+    if (cmdAndZPress) {
+      const check = [...nodes.slice(0, nodes.length - 1)];
+      console.log(check);
+      localStorage.setItem("currentIndex", nodes.length - 1);
+      setNodes(check);
+    }
+    if (cmdAndYPress) {
+      const undoNodes = JSON.parse(localStorage.getItem("nodes"));
+      const check = [...nodes, undoNodes[nodes.length]];
+      console.log(check);
+      localStorage.setItem("currentIndex", check.length - 1);
+      setNodes(check);
+    }
+  }, [cmdAndZPress, cmdAndYPress]);
 
   const onPaneClick = useCallback(
     (event) => {
@@ -44,17 +99,29 @@ const FlowInner = () => {
         data: { label: `Node ${id}` },
       };
 
-      setNodes((nds) => nds.concat(newNode));
+      setNodes((nds) => {
+        const node = nds.concat(newNode);
+        localStorage.setItem("nodes", JSON.stringify(node, null, 2));
+        return node;
+      });
     },
     [nodes, screenToFlowPosition, setNodes]
   );
 
+  const onNodeClick = (event) => {
+    console.log(event);
+  };
+
   return (
-    <div style={{ width: "100vw", height: "100vh" }}>
+    <div
+      style={{ width: "100vw", height: "100vh", pointerEvents: "all" }}
+      onKeyDown={(event) => console.log(event)}
+    >
       {" "}
       {/* Ensure 100vh here */}
       <ReactFlow
         nodes={nodes}
+        onNodeClick={onNodeClick}
         onNodesChange={onNodesChange}
         onPaneClick={onPaneClick} // Native React Flow prop for clicking the background
         nodeTypes={nodeTypes}
